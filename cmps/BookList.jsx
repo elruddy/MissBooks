5bf5e152f95f7b56580741e6cf00c173949ac85f
Lea Guzmicka <lea.guzmicka@outlook.com>
2026-03-05 21:52:43 +0100

    book list css

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -5,23 +5,25 @@ export function BookList({ books, onRemoveBook }) {
 	return (
 		<ul className="book-list container">
 			{books.map((book) => (
-				<li key={book.id}>
-					<BookPreview book={book} />
-					<section className="book-actions">
-						<i
-							className="fas fa-trash"
-							onClick={(ev) => onRemoveBook(book.id, ev)}
-						></i>
+				<Link to={`/book/${book.id}`}>
+					<li key={book.id}>
+						<BookPreview book={book} />
+						<section className="book-actions">
+							<i
+								className="fas fa-trash"
+								onClick={(ev) => onRemoveBook(book.id, ev)}
+							></i>
 
-						<Link to={`/book/${book.id}`}>
-							<i className="fas fa-info-circle"></i>
-						</Link>
+							<Link to={`/book/${book.id}`}>
+								<i className="fas fa-info-circle"></i>
+							</Link>
 
-						<Link to={`/book/edit/${book.id}`}>
-							<i className="fas fa-edit"></i>
-						</Link>
-					</section>
-				</li>
+							<Link to={`/book/edit/${book.id}`}>
+								<i className="fas fa-edit"></i>
+							</Link>
+						</section>
+					</li>
+				</Link>
 			))}
 		</ul>
 	);
